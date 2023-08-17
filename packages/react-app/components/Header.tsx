fa5d1e0f5f91c890dde7a7aa847170b1c0cc3ac5
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-08-17 20:50:47 +0100

    added the explore page

--- packages/react-app/components/Header.tsx
+++ packages/react-app/components/Header.tsx
@@ -45,7 +45,7 @@ export default function Header() {
                 </div>
                 <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
                   <Link
-                    href="#"
+                    href="/explore"
                     className="inline-flex items-center border-b-2 border-black px-1 pt-1 text-sm font-medium text-gray-900"
                   >
                     Explore
