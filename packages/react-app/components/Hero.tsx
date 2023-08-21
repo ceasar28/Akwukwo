790925b2252b393b879729d324b021a33225d1f5
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-08-21 16:28:38 +0100

    added landing page

--- packages/react-app/components/Hero.tsx
+++ packages/react-app/components/Hero.tsx
@@ -1,29 +1,34 @@
 import React from "react";
+import Image from "next/image";
+import illustrations from "../assests/images/illustrations.jpg";
 
-const Hero: React.FC = () => {
+const HeroSection: React.FC = () => {
   return (
-    <section className="py-20 bg-blue-500 text-white">
-      <div className="container mx-auto px-4">
-        <div className="flex flex-col md:flex-row items-center">
-          <div className="md:w-1/2 mb-6 md:mb-0">
-            <h1 className="text-4xl font-bold mb-4">
-              Mint Your Exclusive Book NFTs
-            </h1>
-            <p className="text-lg">
-              Own a piece of literary history through blockchain technology.
-            </p>
-          </div>
-          <div className="md:w-1/2">
-            <img
-              src="/book-illustration.png"
-              alt="Book Illustration"
-              className="w-full h-auto"
-            />
-          </div>
+    <section className="bg-white-800 text-black py-20">
+      <div className="container mx-auto text-center">
+        <h1 className="text-center font-extralight pb-2 md:text-8xl">
+          Akwụkwọ
+        </h1>
+        <p className="text-center text-4xl md:text-5xl font-latoBold pb-6">
+          Bringing the Power of Blockchain for Authors, Creators, and Readers
+        </p>
+        <p className="text-center font-sfProRegular text-gray text-lg xl:px-72 lg:px-6 px-6 mb-16">
+          Akwụkwọ bring the revolutionary advance to claim back the value of
+          digital publishing and library. We want authors to benefit from the
+          see change of NFT technology, and also readers to be able to cherish
+          new and exciting content for the first time.. We are Just at beginning
+          of NFTS Books capabilities
+        </p>
+        <div>
+          <Image
+            src={illustrations}
+            alt="book illsutrations"
+            className="illustration"
+          />
         </div>
       </div>
     </section>
   );
 };
 
-export default Hero;
+export default HeroSection;
--- packages/react-app/pages/index.tsx
+++ packages/react-app/pages/index.tsx
@@ -1,11 +1,11 @@
-import Hero from "../components/Hero";
+import HeroSection from "../components/Hero";
 import HowItWorks from "../components/HowItWorks";
 
 export default function Home() {
   return (
     <div>
       <div className="h1">
-        <Hero></Hero>
+        <HeroSection />
         <HowItWorks />
       </div>
     </div>
