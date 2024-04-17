a15483c9361a3e661c685b096177074b32d14f64
Emmanuel Ekete <66487301+ceasar28@users.noreply.github.com>
2024-04-17 22:11:54 +0100

    Update downsection.tsx

--- packages/react-app/components/downsection.tsx
+++ packages/react-app/components/downsection.tsx
@@ -7,22 +7,22 @@ const FooterSection: React.FC = () => {
       <div className="sm:container sm:m-auto pb-10 pt-20 mx-6">
         <div className="grid grid-cols-footer">
           <div className="col-start-1 col-end-6 md:col-start-auto md:col-end-auto">
-            <h1>Powered by Celo</h1>
-            <img
+            <h1>Powered Klatyn</h1>
+{/*             <img
               width="100px"
               src="https://alfajores.celoscan.io/images/svg/brands/main.svg?v=23.8.2.0"
               alt="Celo"
               className="mb-11 md:mb-0"
-            />
+            /> */}
           </div>
           <div className="col-start-1 col-end-6 md:col-start-auto md:col-end-auto">
-            <h1> and Bunzz</h1>
-            <img
+            <h1> and Hackex</h1>
+{/*             <img
               width="100px"
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTNXUtcigVez4a4Sr_qLhztBdAJIyWDC5fK0j1aiSXGapxawsIJoy1q8G5maLYrL14Y6g&usqp=CAU"
               alt="Bunzz"
               className="mb-11 md:mb-0"
-            />
+            /> */}
           </div>
 
           <div className="mb-11 md:mb-0">
@@ -61,9 +61,9 @@ const FooterSection: React.FC = () => {
         </div>
       </div>
       <div className="border-t border-borderColor py-6 flex justify-center">
-        <p className="font-sfProRegular text-black text-xs pr-5">
+{/*         <p className="font-sfProRegular text-black text-xs pr-5">
           © 2023 Akwukwo. All rights reserved.
-        </p>
+        </p> */}
       </div>
     </section>
   );
